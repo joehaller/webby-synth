663b82154b90cb1c9c782dc83fd13dc4619b76ca
Joseph Haller <j.edward.haller@gmail.com>
2021-03-30 15:21:22 -0400

    Implemented basic keyboard with react

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -12,13 +12,15 @@ class App extends React.Component {
             type : 'square'
           },
           envelope : {
-            attack : 0.005,
+            attack : 0.002,
             decay : 0.1,
-            sustain : 0.9,
-            release : .8
+            sustain : 0.5,
+            release : .6
           }
         },
-      now: Tone.now()
+      now: Tone.now(),
+      octave: 3,
+      poly: false
     }
     this.playNote = this.playNote.bind(this);
     this.stopNote = this.stopNote.bind(this);
@@ -34,17 +36,34 @@ class App extends React.Component {
 
 
   playNote(e) {
+    let oct = this.state.octave;
     switch (e.key) {
       case "a":
-        return this.state.synth.triggerAttack(`C4`, this.state.now);
+        return this.state.synth.triggerAttack(`C${oct}` , this.state.now);
       case "w":
-        return this.state.synth.triggerAttack(`C#4`, this.state.now);
+        return this.state.synth.triggerAttack(`C#${oct}`, this.state.now);
       case "s":
-        return this.state.synth.triggerAttack(`D4`, this.state.now);
+        return this.state.synth.triggerAttack(`D${oct}`, this.state.now);
       case "e":
-        return this.state.synth.triggerAttack(`D#4`, this.state.now);
+        return this.state.synth.triggerAttack(`D#${oct}`, this.state.now);
       case "d":
-        return this.state.synth.triggerAttack(`E4`, this.state.now);
+        return this.state.synth.triggerAttack(`E${oct}`, this.state.now);
+      case "f":
+      return this.state.synth.triggerAttack(`F${oct}`, this.state.now);
+      case "t":
+      return this.state.synth.triggerAttack(`F#${oct}`, this.state.now);
+      case "g":
+      return this.state.synth.triggerAttack(`G${oct}`, this.state.now);
+      case "y":
+      return this.state.synth.triggerAttack(`G#${oct}`, this.state.now);
+      case "h":
+      return this.state.synth.triggerAttack(`A${oct}`, this.state.now);
+      case "u":
+      return this.state.synth.triggerAttack(`A#${oct}`, this.state.now);
+      case "j":
+      return this.state.synth.triggerAttack(`B${oct}`, this.state.now);
+      case "k":
+      return this.state.synth.triggerAttack(`C${oct+1}`, this.state.now);
       default:
         return;
     }
@@ -57,8 +76,15 @@ class App extends React.Component {
       case "s":
       case "e":
       case "d":
-
-         this.state.synth.triggerRelease();
+      case "f":
+      case "t":
+      case "g":
+      case "y":
+      case "h":
+      case "u":
+      case "j":
+      case "k":
+        this.state.synth.triggerRelease();
     }
   }
 
@@ -79,6 +105,30 @@ class App extends React.Component {
         </div>
         <div className="key-wrapper">
           <button className="key" onKeyUp={this.stopNote} onKeyDown={this.playNote}>E</button>
+        </div>
+        <div className="key-wrapper">
+          <button className="key" onKeyUp={this.stopNote} onKeyDown={this.playNote}>F</button>
+        </div>
+        <div className="key-wrapper">
+          <button className="key" onKeyUp={this.stopNote} onKeyDown={this.playNote}>F#</button>
+        </div>
+        <div className="key-wrapper">
+          <button className="key" onKeyUp={this.stopNote} onKeyDown={this.playNote}>G</button>
+        </div>
+        <div className="key-wrapper">
+          <button className="key" onKeyUp={this.stopNote} onKeyDown={this.playNote}>G#</button>
+        </div>
+        <div className="key-wrapper">
+          <button className="key" onKeyUp={this.stopNote} onKeyDown={this.playNote}>A</button>
+        </div>
+        <div className="key-wrapper">
+          <button className="key" onKeyUp={this.stopNote} onKeyDown={this.playNote}>A#</button>
+        </div>
+        <div className="key-wrapper">
+          <button className="key" onKeyUp={this.stopNote} onKeyDown={this.playNote}>B</button>
+        </div>
+        <div className="key-wrapper">
+          <button className="key" onKeyUp={this.stopNote} onKeyDown={this.playNote}>C</button>
         </div>
       </div>
     )
